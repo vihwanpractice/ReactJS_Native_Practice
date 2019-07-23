b8af6f9d73df4bc1009d37ceaa8134af4d34abf7
vihwan <vihwan@ajou.ac.kr>
2019-07-23 15:28:42 +0900

    Add ScrollView function

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { Constants } from 'expo';
 export default class App extends React.Component {
   render() {
     return (
+      <ScrollView style={styles.container}>
       <View style={styles.container}>
         <Text style={{fontSize:96}}>Scroll me plz</Text>
         <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
@@ -19,6 +20,7 @@ export default class App extends React.Component {
         <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
         <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
       </View>
+      </ScrollView>
     );
   }
 }
@@ -27,9 +29,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    marginTop: Constants.statusBarHeight,
-
-    borderColor: 'red',
-    borderWidth: 2,
   },
 });
