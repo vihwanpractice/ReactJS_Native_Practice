b5f65359bf5752a9f2a775621e82a5c0c21bf8ea
vihwan <vihwan@ajou.ac.kr>
2019-07-23 14:43:55 +0900

    Add More Components : CounterButton

--- App.js
+++ App.js
@@ -31,6 +31,9 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <CounterButton/>
+        <CounterButton />
+        <CounterButton />
+        <CounterButton />
       </View>
     )
   }
