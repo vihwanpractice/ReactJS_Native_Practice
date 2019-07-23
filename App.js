a2903de4bde7664268c48942a2c52e38061bcad6
vihwan <vihwan@ajou.ac.kr>
2019-07-23 15:35:46 +0900

    Modify ScrollView Import

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, ScrollView } from 'react-native';
 import { Constants } from 'expo';
 
 export default class App extends React.Component {
   render() {
     return (
       <ScrollView style={styles.container}>
-      <View style={styles.container}>
         <Text style={{fontSize:96}}>Scroll me plz</Text>
         <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
         <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
@@ -19,7 +18,6 @@ export default class App extends React.Component {
         <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
         <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
         <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-      </View>
       </ScrollView>
     );
   }
@@ -29,5 +27,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    marginTop: Constants.statusBarHeight,
+
+
+    borderColor: 'red',
+    borderWidth: 2,
   },
 });
